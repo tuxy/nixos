b5f6f00c8ab2b522c377e97534727cb8b9195403
tuxy <lastpass7565@gmail.com>
2024-12-25 09:17:32 +0700

    Fixed lsps and added cmp

--- home-manager/neovim/lsp.nix
+++ home-manager/neovim/lsp.nix
@@ -6,27 +6,34 @@
   # Installing lsp s
   home.packages = with pkgs; [
     rust-analyzer
-    pylyzer 
+    pylyzer
     nil
     ccls
   ];
 
   # Enable lsp and lsp-format in nixvim
   programs.nixvim.plugins = {
+    # Add auto-completion for neovim
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+    };
+    # Enabling lsp's for neovim
     lsp = {
       enable = true;
       servers = {
         rust_analyzer = {
           enable = true;
-	  installCargo = true;
-	  installRustc = true;
-	  installRustfmt = true;
-	};
+          installCargo = true;
+          installRustc = true;
+          installRustfmt = true;
+        };
         ccls.enable = true;
         nil_ls.enable = true;
         pylyzer.enable = true;
       };
     };
     lsp-format.enable = true;
+    lualine.enable = true;
   };
 }
